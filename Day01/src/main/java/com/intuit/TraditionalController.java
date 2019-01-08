e2f53cd1215a7d6b643ac1c787e671b7a4723383
prabhu-durasoft <iamprabhu@yahoo.com>
2019-01-08 12:14:17 +0530

    day01

--- Day01/src/main/java/com/intuit/TraditionalController.java
+++ Day01/src/main/java/com/intuit/TraditionalController.java
@@ -1,11 +1,30 @@
 package com.intuit;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TraditionalController {
 
+	@PostMapping("/process")
+	public String processForm(@RequestParam("firstname") String firstName, 
+				HttpSession session) {
+		String message = "Hello " + firstName;
+		session.setAttribute("message", message);
+		return "result";
+	}
+	
+//	@PostMapping("/process")
+//	@ResponseBody
+//	public String processForm(@RequestParam("firstname") String firstName) {
+//		return "Hello " + firstName;
+//	}
+	
 	@GetMapping("/start")
 	public String start() {
 		return "index";
--- Day01/src/main/webapp/index.jsp
+++ Day01/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 	<body>
 		<h1>Welcome to index.jsp</h1>
+		<form action="/process" method="POST">
+			<input type="text" name="firstname"> <br/>
+			<button>Click</button>
+		</form>
 	</body>
 </html>
