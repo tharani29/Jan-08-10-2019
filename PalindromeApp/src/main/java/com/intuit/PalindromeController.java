951768e1ac420e6afea10bf241c44e48cd7b0481
prabhu-durasoft <iamprabhu@yahoo.com>
2019-01-10 14:33:11 +0530

    Config

--- PalindromeApp/src/main/java/com/intuit/PalindromeController.java
+++ PalindromeApp/src/main/java/com/intuit/PalindromeController.java
@@ -1,6 +1,7 @@
 package com.intuit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,17 @@ public class PalindromeController {
 	@Autowired
 	private Palindrome palindrome;
 	
+	@Value("${welcome}")
+	private String welcome;
+	
 	@GetMapping("/palindrome/{word}")
 	public boolean check(@PathVariable String word) {
 		return palindrome.check(word);
 	}
 	
+	
+	@GetMapping("/")
+	public String index() {
+		return welcome;
+	}
 }
