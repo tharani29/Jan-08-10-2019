05985bd0b1f913dca6dd2d7c47eca70993f4700a
prabhu-durasoft <iamprabhu@yahoo.com>
2019-01-10 11:32:17 +0530

    UI

--- UI/src/main/java/com/intuit/UIController.java
+++ UI/src/main/java/com/intuit/UIController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 @Controller
 public class UIController {
 
@@ -20,14 +23,42 @@ public class UIController {
 	
 	@GetMapping("/anagram/{word}")
 	@ResponseBody
+	@HystrixCommand(fallbackMethod="getAnagramFallback")
 	public String getAnagram(@PathVariable String word) {
 		return wordUtil.generateAnagram(word);
 	}
 	
+	public String getAnagramFallback(String word) {
+		return "Oops! Cannot serve you now. Try later";
+	}
+	
 	@GetMapping("/palindrome/{word}")
 	@ResponseBody
+	@HystrixCommand(fallbackMethod="getPalindromeFallback"
+		,commandProperties= {
+				@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="4000")
+		})
 	public String getPalindrome(@PathVariable String word) {
+		System.out.println("***calling getPalindrome method");
 		return wordUtil.checkPalindrome(word);
 	}
+	
+	public String getPalindromeFallback(String word) {
+		System.out.println("--in getPalindromeFallback method");
+		return "Oops! Cannot serve you now. Try later";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- UI/src/main/java/com/intuit/UiApplication.java
+++ UI/src/main/java/com/intuit/UiApplication.java
@@ -2,8 +2,12 @@ package com.intuit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class UiApplication {
 
 	public static void main(String[] args) {
